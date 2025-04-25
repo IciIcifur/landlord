e41f5a59f60aa4db989213f93c61365cef8cb185
IciIcifur <iciicifur@yandex.ru>
2025-04-25 14:13:03 +0300

    Starting a project

---
+++ tailwind.config.ts
@@ -0,0 +1,41 @@
+import type {Config} from 'tailwindcss';
+import {heroui} from '@heroui/theme';
+
+export default {
+    content: [
+        './(pages)/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        '../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: 'class',
+    plugins: [
+        heroui({
+            addCommonColors: true,
+            themes: {
+                light: {
+                    /*
+                    colors: {
+                        background: '#fafafa',
+                        foreground: '#18181b',
+                        primary: primary,
+                        secondary: secondary,
+                        danger: danger,
+                        warning: warning,
+                        success: success,
+                    },*/
+                },
+                dark: {
+                    /*
+                    colors: {
+                        background: '#18181b',
+                        foreground: '#fafafa',
+                    },*/
+                },
+            },
+        }),
+    ],
+} satisfies Config;
