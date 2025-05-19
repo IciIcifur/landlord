6a56c5cd7477d3d0b3efd33ba6a8f4acf42b3288
Astashonok Egor <91051834+AEV26@users.noreply.github.com>
2025-05-19 21:41:33 +0300

    Update definitions.ts
    Add new types

--- app/lib/utils/definitions.ts
+++ app/lib/utils/definitions.ts
@@ -1,7 +1,50 @@
 export interface User {
   id: string;
   email: string;
+  password: string;
   role: UserRole;
+  object: Object[];
+}
+
+export interface Object {
+  id: string; 
+  name: string;
+  size: number;
+  description?: string;
+  records: Record[];
+  dataForSale: DataForSale[];
+}
+
+export interface Record {
+  id: string;
+  date: string;
+  rent?: number;
+  heat?: number;
+  explotation?: number;
+  mop?: number;
+  renovation?: number;
+  tbo?: number;
+  electricity?: number;
+  earthRent?: number;
+  otherExpenses?: number;
+  otherIncomes?: number;
+  security?: number;
+  totalExpenses?: number;
+  totalIncomes?: number;
+  totalProfit?: number;
+}
+
+export interface DataForSale {
+  id: string;
+  purchasePrice: number;
+  priceForSale: number;
+  countOfMonth?: number;
+  profitPerMonth?: number;
+  totalProfit?: number;
+  payback5Year?: number;
+  payback7Year?: number;
+  payback10Year?: number;
+  percentPerYear?: number;
 }
 
 export enum UserRole {
