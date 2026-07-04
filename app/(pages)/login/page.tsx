fe4c744b3e6c3b4b090bafa1dbb12f4b9f200279
IciIcifur <iciicifur@yandex.ru>
2026-07-04 20:25:59 +0300

    Demo alert with pass keys

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,15 +1,32 @@
 import LoginForm from '@/app/ui/forms/loginForm';
 import { Card, CardBody } from '@heroui/card';
+import { Alert } from '@heroui/alert';
+
+const MODE = process.env.MODE;
+const USERNAME = process.env.SHOWCASE_USERNAME;
+const PASSWORD = process.env.SHOWCASE_PASSWORD;
 
 export default function LoginPage() {
+  const alertTitle = `Приложение в режиме демонстрации`;
+  const alertDescription = `Войдите с логином ${USERNAME} и паролем ${PASSWORD}.`;
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center">
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-8">
       <Card>
-        <CardBody className="flex max-w-sm flex-col items-start justify-center gap-4 p-8">
+        <CardBody className="flex w-full max-w-sm flex-col items-start justify-center gap-4 p-8">
           <h1 className="text-xl font-medium">Войдите, чтобы продолжить</h1>
           <LoginForm />
         </CardBody>
       </Card>
+
+      {MODE === 'SHOWCASE' && (
+        <Alert
+          hideIcon
+          color="primary"
+          className="max-w-sm flex-grow-0"
+          description={alertDescription}
+          title={alertTitle}
+        />
+      )}
     </div>
   );
 }
